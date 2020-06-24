93c902138cdaaba2867a0214e985334407044998
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2020-06-24 14:44:15 -0400

    Update word_frequency.py

--- word_frequency.py
+++ word_frequency.py
@@ -7,9 +7,11 @@ STOP_WORDS = [
 
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
+    # Your code will go here. You can write additional functions if you want, and call them inside this function.
     pass
 
 
+# This is an "incantation." You will not see it very often, and it needs to be here to be able to pass file names as arguments.
 if __name__ == "__main__":
     import argparse
     from pathlib import Path
